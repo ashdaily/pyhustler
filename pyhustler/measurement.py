1ac7d8bbc9b20a54867ebfcd9f847a822f2168a3
ashdaily <ashishsinghbardhan@gmail.com>
2020-11-03 03:04:01 +0900

    add measurement

---
+++ pyhustler/measurement.py
@@ -0,0 +1,34 @@
+__all__ = ["MEASUREMENT_NAMES", "MEASUREMENT_UNITS"]
+
+
+MEASUREMENT_NAMES = (
+        ("LENGTH", "LENGTH"),
+        ("WIDTH", "WIDTH"),
+        ("HEIGHT", "HEIGHT"),
+        ("WEIGHT", "WEIGHT"),
+        ("WAIST", "WAIST"),
+        ("BUST", "BUST"),
+        ("CHEST", "CHEST"),
+        ("RADIUS", "RADIUS"),
+        ("DIAMETER", "DIAMETER"),
+        ("VOLUME", "VOLUME"),
+        ("SHOULDER", "SHOULDER"),
+        ("COLLAR", "COLLAR"),
+        ("SLEEVE", "SLEEVE"),
+        ("CUFF", "CUFF"),
+        ("THICKNESS", "THICKNESS"),
+        ("INSEAM", "INSEAM")
+)
+
+
+MEASUREMENT_UNITS = (
+        ("AMPERE", "AMPERE"),
+        ("CELCIUS", "CELCIUS"),
+        ("CENTIMETER", "CENTIMETER"),
+        ("GALLON", "GALLON"),
+        ("KELVIN", "KELVIN"),
+        ("METER", "METER"),
+        ("MILLIMETER", "MILLIMETER"),
+        ("LITRE", "LITRE"),
+        ("MILLILITRE", "MILLILITRE")
+)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyhustler-ashdaily",
-    version="0.3.0",
+    version="0.4.0",
     author="Ashish Singh Bardhan",
     author_email="ashtokyo31@gmail.com",
     description="Useful tools for daily python/django hustles",
